b5162923cb3c69b361b4af2c3153ab6eda5e2217
MouhcineR0 <rachidmouhcine88@gmail.com>
2024-12-22 22:48:31 +0100

    small issue in atoi

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: rmouhcin <rmouhcin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 17:14:32 by rmouhcin          #+#    #+#             */
-/*   Updated: 2024/12/10 11:13:10 by rmouhcin         ###   ########.fr       */
+/*   Updated: 2024/12/22 22:40:53 by rmouhcin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,23 +30,19 @@ long long	ft_atoi(const char *str)
 {
 	int			sign;
 	long long	result;
-	long long	history;
 
 	result = 0;
 	sign = 1;
-	history = 0;
 	while ((*str >= 9 && *str <= 13) || *str == ' ')
 		str++;
 	sign = getsign(&str);
 	while (ft_isdigit((int)*str))
 	{
 		result = result * 10 + *str - '0';
-		if (history != result / 10 && sign == -1)
-			return (0);
-		else if (history != result / 10 && sign == 1)
-			return (-1);
+		if ((result > INT32_MAX && sign == 1)
+			|| (result < INT32_MIN && sign == -1))
+			return ((long)INT32_MAX + 1);
 		str++;
-		history = result;
 	}
 	return (sign * result);
 }
